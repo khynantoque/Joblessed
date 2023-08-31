8832121c921ea9887bcb1d0cc905041a05337d13
Khyn Antoque <khynantoque@gmail.com>
2023-09-01 02:43:26 +0800

    Test Urls

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from app import views
+
+urlpatterns = [
+    path('', views.job_list),
+    path('job/<int:id>', views.job_detail)
+]
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+jobs = [
+    'First Job',
+    'Second Job',
+    'Third Job'
+]
+
+details = [
+    'First Description',
+    'Second Description',
+    'Third Description'
+]
+
+
 # Create your views here.
+def job_list(request):
+    html = ''
+    for i, j in enumerate(jobs):
+        html += f'<li><a href="job/{i}">{j}</a></li>'
+    return HttpResponse(f'<html><ol>{html}</ol></html>')
+
+
+def job_detail(request, id):
+    return HttpResponse(f'<h1>{jobs[id]}</h1><h3>{details[id]}</h3>')
