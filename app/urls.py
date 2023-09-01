81264994ff18724a92487dea2769055cafee3888
Khyn Antoque <khynantoque@gmail.com>
2023-09-01 23:51:23 +0800

    Added Error Handling

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.job_list),
-    path('job/<int:id>', views.job_detail)
+    path('', views.job_list, name='jobs_list'),
+    path('job/<int:jobid>', views.job_detail, name='jobs_detail')
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from django.urls import reverse
+from django.shortcuts import redirect
 
 jobs = [
     'First Job',
@@ -18,9 +19,14 @@ details = [
 def job_list(request):
     html = ''
     for i, j in enumerate(jobs):
-        html += f'<li><a href="job/{i}">{j}</a></li>'
+        html += f'<li><a href="{reverse("jobs_detail", args=(i,))}">{j}</a></li>'
     return HttpResponse(f'<html><ol>{html}</ol></html>')
 
 
-def job_detail(request, id):
-    return HttpResponse(f'<h1>{jobs[id]}</h1><h3>{details[id]}</h3>')
+def job_detail(request, jobid):
+    try:
+        if jobid == 0:
+            return redirect(reverse('jobs_list'))
+        return HttpResponse(f'<h1>{jobs[jobid]}</h1><h3>{details[jobid]}</h3>')
+    except:
+        return HttpResponseNotFound('Not Found')
