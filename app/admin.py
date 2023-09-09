13b8ea1194370e13be0c1aab1a4c3020c8ad3d9b
Khyn Antoque <khynantoque@gmail.com>
2023-09-09 08:36:22 +0800

    Added Location for jobs

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,20 @@ from app.models import JobPost
 
 class JobAdmin(admin.ModelAdmin):
     list_display = ('title', 'salary', 'date')
+    list_filter = ('date', 'salary', 'expiry')
+    search_fields = ('title', 'description')
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'description', 'salary')
+        }),
+        ('Additional Information', {
+            'classes': ('collapse',),
+            'fields': ('expiry', 'slug')
+        }),
+    )
 
 # Register your models here.
-admin.site.register(JobPost)
+admin.site.register(JobPost, JobAdmin)
+admin.site.site_header = 'JoBlessed Admin'
+admin.site.site_title = 'JobBlessed Portal'
+admin.site.index_title = 'Welcome user!'
--- app/models.py
+++ app/models.py
@@ -3,6 +3,14 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Location(models.Model):
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    zip = models.IntegerField()
+    
+
 class JobPost(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
@@ -10,6 +18,7 @@ class JobPost(models.Model):
     salary = models.IntegerField()
     expiry = models.DateField(null=True)
     slug = models.SlugField(null=True, max_length=40, unique=True)
+    location = models.OneToOneField(Location, on_delete=models.CASCADE, null=True)
     
     def save(self, *args, **kwargs):
         if not self.id:
